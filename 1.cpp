bbc5363db80c0cd5efbdc9021785539893529b39
gusgatam <gustavo.gatamora@gmail.com>
2020-12-04 14:35:03 -0300

    Devolución.
    Comentarios en el primer ejercicio, terminar de subir los trabajos adeudados.

--- 1.cpp
+++ 1.cpp
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(){
-int a,rtt=0,rt[]{1,2,3,4,5};
-for (a=0;a<5;a++){
+int main()//faltan las fuciones.
+{
+int a,rtt=0,rt[5]{0};//Vector inicializado en 0
+for (a=0;a<5;a++)
+{
 printf("Ingrese las resistencias ");
-scanf("%d",&rt[a]);}
-for (a=0;a<5;a++){
+scanf("%d",&rt[a]);
+}
+for (a=0;a<5;a++)
+{
 printf("\n Las resistencias son %d",rt[a]);
 rtt=rtt+rt[a];
 }
